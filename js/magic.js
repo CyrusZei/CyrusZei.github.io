6e9a1f2b6cdcb110791d71651eccc2093d915518
Cyrus Zei <cyruszei@gmail.com>
2016-03-31 16:14:20 +0200

    fixed bug

---
+++ js/magic.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+  $('#sidebar-btn').click(function() {
+    $('#sidebar').toggleClass('visible');
+  });
+  // Operator
+  $('#operator').click(function() {
+    $('#operator_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#operator_show').fadeOut('slow');
+  });
+
+  //modric
+  $('#modric').click(function() {
+    $('#modric_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#modric_show').fadeOut('slow');
+  });
+
+  $('#biluthyrning').click(function() {
+    $('#biluthyrning_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#biluthyrning_show').fadeOut('slow');
+  });
+
+  $('#jobstream').click(function() {
+    $('#jobstream_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#jobstream_show').fadeOut('slow');
+  });
+
+  $('#kundwebb').click(function() {
+    $('#kundwebb_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#kundwebb_show').fadeOut('slow');
+  });
+
+  $('#standbytid').click(function() {
+    $('#standbytid_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#standbytid_show').fadeOut('slow');
+  });
+
+  $('#suntropez').click(function() {
+    $('#suntropez_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#suntropez_show').fadeOut('slow');
+  });
+
+  $('#ptplym').click(function() {
+    $('#ptplym_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#ptplym_show').fadeOut('slow');
+  });
+  $('#itunes').click(function() {
+    $('#itunes_show').fadeIn('slow');
+  });
+  $('.close').click(function() {
+    $('#itunes_show').fadeOut('slow');
+  });
+
+
+
+
+
+
+
+
+});
